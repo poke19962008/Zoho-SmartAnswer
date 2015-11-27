9a18355830652de823d4723be7730e0d2fb55f70
Sayan Das <poke19962008@gmail.com>
2015-11-27 09:48:49 +0530

    bug fixes and added except. handling

--- scrapper/main.py
+++ scrapper/main.py
@@ -9,54 +9,58 @@ url = root + str(studentID)
 
 connected = False
 try: 
-	con = pymongo.Connect(host=mongoURI, port=27017)
+	con = pymongo.Connection(host=mongoURI, port=27017)
 	db = con['zoho']
 	connected = True
 except: 
 	print "[Error] Cannot connect with mongodb."
-
+errorLogger = open('errLog.txt', 'a')
 
 while True and connected:
-	soup = BeautifulSoup(urllib2.urlopen(root + str(studentID)).read())
-	table = soup.find_all('table')
-	student = {}
+	try:
+		soup = BeautifulSoup(urllib2.urlopen(root + str(studentID)).read())
+		table = soup.find_all('table')
+		student = {}
 
-	# For the credential of the student
-	indexer = 1
-	keyDict = {"Program/Dept": "dept", "Name of the Student": "name", "Registration Number": "_id"}
-	for td in table[0].find_all('td')[2:14]:
-		if indexer % 4 == 0:
-			indexer = 1
-			student[key] = val
+		# For the credential of the student
+		indexer = 1
+		keyDict = {"Program/Dept": "dept", "Name of the Student": "name", "Registration Number": "_id", "Semester": "semester"}
+		for td in table[0].find_all('td')[2:14]:
+			if indexer % 4 == 0:
+				indexer = 1
+				student[key] = val
 
-		if indexer == 1:
-			key = keyDict[td.get_text()]
-		elif indexer == 3:
-			val = td.get_text()
-		
-		indexer = indexer + 1
+			if indexer == 1:
+				key = keyDict[td.get_text()]
+			elif indexer == 3:
+				val = td.get_text()
+			
+			indexer = indexer + 1
 
-	if student['dept'] != "B.Tech Computer Science and Engineering":
-		break
+		if student['dept'] != "B.Tech Computer Science and Engineering":
+			break
 
-	indexer = 1
-	student['course'] = {}
-	tableCol = {2: "tile", 3: "faculty", 4: "ct1", 5: "ct2", 6: "qat", 7: "st", 8: "mt", 9: "pt", 10: "attendance", 11: "internal"}
-	for td in table[0].find_all('td')[25:-5]:
-		if indexer % 12 == 0:
-			indexer = 1
+		indexer = 1
+		student['course'] = {}
+		tableCol = {2: "tile", 3: "faculty", 4: "ct1", 5: "ct2", 6: "qat", 7: "st", 8: "mt", 9: "pt", 10: "attendance", 11: "internal"}
+		for td in table[0].find_all('td')[25:-5]:
+			if indexer % 12 == 0:
+				indexer = 1
 
-		if indexer == 1:
-			sCode = td.get_text()
-			student['course'][sCode] = {}
-		elif td.get_text() != "":
-			student['course'][sCode][tableCol[indexer]] = td.get_text()
+			if indexer == 1:
+				sCode = td.get_text()
+				student['course'][sCode] = {}
+			elif td.get_text() != "":
+				student['course'][sCode][tableCol[indexer]] = td.get_text()
 
-		indexer = indexer + 1
-	print "[Success] Scrapped " + student["Name of the Student"]
+			indexer = indexer + 1
+		print "[Success] Scrapped " + student["name"] + " studentID= " + str(studentID)
 
-	db.main.insert(student)
+		db.main.insert(student)
+	except: 
+		print "[Error] Cannot scrap studentID: " + str(studentID)
+		errorLogger.write("{\"studentID\": "+ str(studentID) +"}\n")
+
 	studentID = studentID + 4
-
 
 
