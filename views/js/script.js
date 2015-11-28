3ba7133877197c0ffc5a11dc42dc29f2dce955ca
Sayan Das <poke19962008@gmail.com>
2015-11-28 17:15:30 +0530

    initial front end

---
+++ views/js/script.js
@@ -0,0 +1,10 @@
+var alertMsg = ["How much I scored in oops, micro and ooad",
+               "How much I scored in dcf"];
+
+$(document).ready(function (){
+
+  setInterval(function(){
+    var ind = Math.floor((Math.random() * (alertMsg.length-1)) );
+    notie.alert(4, "Search for \'"+alertMsg[ind]+"\'" , 3);
+  }, 5000);
+});
